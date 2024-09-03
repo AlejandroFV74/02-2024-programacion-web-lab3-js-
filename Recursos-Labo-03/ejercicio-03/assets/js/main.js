fa7cdf000bad645bca594f026a0739457635054b
AlejandroFV74 <00067323@uca.edu.sv>
2024-09-03 15:41:39 -0600

    Ejercicio 3 corregido

--- Recursos-Labo-03/ejercicio-03/assets/js/main.js
+++ Recursos-Labo-03/ejercicio-03/assets/js/main.js
@@ -1,9 +1,9 @@
 const calculateFactorial = (number) => {
-    let iterator = number;
-    let result = number;
-    while (iterator !=0) {
+    if (number < 0) return "El número debe ser entero no negativo";
+    let result = 1;
+    while (number>0) {
         result*=number;
-        iterator--;
+        number--;
     }
     return result;
 
@@ -11,15 +11,30 @@ const calculateFactorial = (number) => {
 
 const requestNumber = () => {
     /* Numero entero no negativo */
-    let num1= prompt('Ingrese un numero');
-    return num1;
+    while (true) {
+        let num1 = prompt('Ingrese un número entero no negativo');
+
+        if (num1 === null) {
+            alert("Entrada cancelada por el usuario.");
+            return null; 
+        }
+
+        const parsed = parseInt(num1, 10);
+
+        if (!isNaN(parsed) && parsed >= 0) {
+            return parsed;
+        }
+
+        alert("Por favor, ingrese un número entero no negativo válido.");
+    }
+    
     
 }
 
 const main = () => {
     const number= requestNumber();
-    calculateFactorial();
-    
+    const factorial= calculateFactorial(number);
+    alert(`El factorial de ${number} es ${factorial}`);
 }
 
 main();
